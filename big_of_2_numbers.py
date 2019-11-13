9063eee847a7021fffb78c5cf12619d35a1a4637
AK607 <57707394+AK607@users.noreply.github.com>
2019-11-13 16:40:23 +0530

    Add files via upload

---
+++ big_of_2_numbers.py
@@ -0,0 +1,11 @@
+##PROGRAM TO CHECK WHICH OF THE TWO NUMBERS IS GREATER##
+
+#i am using green text is for explanation,this does not effect the program, it will work even when the green text is not included
+
+
+a=int(input("enter the 1st number:")) #taking the first number as 'a'
+b=int(input("enter the 2nd number:")) #taking the second number as 'b'
+if a>b:
+    print(a," is greater")    #prints this when 'a'' is larger
+else:
+    print(b,"is greater")     #prints this when 'b'' is larger
